930c26b6186f5dde6400beb73dea8c8e9ce2dd05
yeontan0826 <yeontan0826@gmail.com>
2024-01-14 18:59:51 +0900

    change packageName 'com.chat_app -> com.dev.chatapp'

--- android/app/src/main/java/com/chat_app/MainActivity.kt
+++ android/app/src/main/java/com/chat_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.chat_app
+package com.dev.chatapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/chat_app/MainApplication.kt
+++ android/app/src/main/java/com/chat_app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.chat_app
+package com.dev.chatapp
 
 import android.app.Application
 import com.facebook.react.PackageList
